2e2229145e7da13f62bcb9e0a18da35b1d21c2ee
mikebell88008@gmail.com <mikebell88008@gmail.com>
2020-09-13 11:20:44 -0700

    completed presentational components

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'passenger-count',
+  templateUrl: 'passenger-count.component.html'
+})
+
+export class PassengerCountComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'passenger-detail',
+  templateUrl: 'passenger-detail.component.html'
+})
+
+export class PassengerDetailComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
