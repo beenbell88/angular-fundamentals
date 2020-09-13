025f43eda40b29282de8d5d62b4878976a0cbed7
mikebell88008@gmail.com <mikebell88008@gmail.com>
2020-09-13 09:36:48 -0700

    completed feature module

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 @NgModule({
   imports: [
+    // angular modules
     BrowserModule,
     FormsModule,
+    // custom modules
+    PassengerDashboardModule
   ],
   bootstrap: [
     AppComponent
