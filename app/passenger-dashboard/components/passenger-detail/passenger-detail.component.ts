f5e9fef82aee957529f297fe0aa59f64856fb0c3
mikebell88008@gmail.com <mikebell88008@gmail.com>
2020-09-14 20:13:31 -0700

    completed input with ngFor

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-detail',
+  styleUrls: ['passenger-detail.component.scss'],
   templateUrl: 'passenger-detail.component.html'
 })
 
-export class PassengerDetailComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit() { }
+export class PassengerDetailComponent {
+  @Input() detail: Passenger;
 }
