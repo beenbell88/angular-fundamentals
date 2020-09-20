d402b30a4bfe1ee95c889d05df7d360b0f2a437c
mikebell88008@gmail.com <mikebell88008@gmail.com>
2020-09-20 12:54:26 -0700

    completed Http put and delete

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -25,20 +25,31 @@ export class PassengerDashboardComponent implements OnInit {
   constructor(private passengerService: PassengerDashboardService) {}
 
   ngOnInit() {
-   this.passengerService.getPassengers()
-   .subscribe((data: Passenger[]) => this.passengers = data);
+    this.passengerService
+      .getPassengers()
+      .subscribe((data: Passenger[]) => (this.passengers = data));
   }
 
   handleEdit(event: Passenger) {
-    this.passengers = this.passengers.map((passenger: Passenger) => {
-      if (passenger.id === event.id) {
-        passenger = Object.assign({}, passenger, event);
-      }
-      return passenger;
-    });
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+          if (passenger.id === event.id) {
+            passenger = Object.assign({}, passenger, event);
+          }
+          return passenger;
+        });
+      });
   }
 
   handleRemove(event: Passenger) {
-    this.passengers = this.passengers.filter((passenger: Passenger) => passenger.id !== event.id);
+    this.passengerService
+      .removePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passengers = this.passengers.filter(
+          (passenger: Passenger) => passenger.id !== event.id
+        );
+      });
   }
 }
